120ebf30ff05b1a438ec9ac8760945e76a4eca95
jakub <jakubstankowski@protonmail.com>
2019-10-29 14:46:53 +0100

    Add Spring Boot to project

---
+++ src/test/java/com/standev/lastminute/LastminuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.standev.lastminute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastminuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
