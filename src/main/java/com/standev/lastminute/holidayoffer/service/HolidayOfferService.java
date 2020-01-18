04f16b413b78d590502c6d79cb773024eae1586d
jakub <jakubstankowski@protonmail.com>
2020-01-18 15:19:43 +0100

    Fixed bugs with holiday offer

--- src/main/java/com/standev/lastminute/holidayoffer/service/HolidayOfferService.java
+++ src/main/java/com/standev/lastminute/holidayoffer/service/HolidayOfferService.java
@@ -2,6 +2,7 @@ package com.standev.lastminute.holidayoffer.service;
 import com.standev.lastminute.holidayoffer.dao.HolidayOfferDAO;
 import com.standev.lastminute.holidayoffer.jpa.HolidayOffer;
 import com.standev.lastminute.holidaypreferences.dao.HolidayPreferencesDAO;
+import com.standev.lastminute.holidaypreferences.jpa.HolidayPreferences;
 import com.standev.lastminute.user.dao.UserDAO;
 import com.standev.lastminute.user.jpa.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +17,14 @@ public class HolidayOfferService {
 
     @Autowired
     HolidayOfferDAO holidayOfferDAO;
+    @Autowired
     HolidayPreferencesDAO holidayPreferencesDAO;
+    @Autowired
     UserDAO userDAO;
 
     public HolidayOffer createHolidayOffer(HolidayOffer holidayOffer, Integer id, Integer userId) {
-            User user = userDAO.findById(userId).orElse(null);
-
-        System.out.println("user finddddd"+ user.getFirstName() + user.getId());
-
-
+        HolidayPreferences holidayPreferences = holidayPreferencesDAO.findByIdAndUserId(id, userId).orElse(null);
+        holidayOffer.setHolidayPreferences(holidayPreferences);
         return holidayOfferDAO.save(holidayOffer);
     }
 
--- src/main/java/com/standev/lastminute/holidayoffer/web/HolidayOfferController.java
+++ src/main/java/com/standev/lastminute/holidayoffer/web/HolidayOfferController.java
@@ -28,6 +28,7 @@ public class HolidayOfferController {
     @PostMapping("/{userId}/holiday-preferences/{id}/holiday-offer")
     public HolidayOffer createHolidayPreferences(@RequestBody HolidayOffer holidayOffer, @PathVariable("userId") Integer userId, @PathVariable("id") Integer id) {
         System.out.println("holiday prefe id: " + id);
+        System.out.println("useeeeeeeeer id" + userId);
 
         return holidayOfferService.createHolidayOffer(holidayOffer, id, userId);
 
