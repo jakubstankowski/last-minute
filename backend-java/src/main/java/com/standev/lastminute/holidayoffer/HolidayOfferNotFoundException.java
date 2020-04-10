d4c59fae65d56da7719e0ee3d332c6345eb2a538
jakub <jakubstankowski@protonmail.com>
2020-04-10 20:10:02 +0200

    change project structure

--- backend-java/src/main/java/com/standev/lastminute/holidayoffer/HolidayOfferNotFoundException.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.standev.lastminute.HolidayOffer;
-
-public class HolidayOfferNotFoundException extends RuntimeException {
-    public HolidayOfferNotFoundException(Integer id){
-        super("Holiday Offer with id: " + id + " not found!");
-    }
-}
-*/
--- backend-java/src/main/java/com/standev/lastminute/user/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.standev.lastminute.user;
-
-public class UserNotFoundException extends  RuntimeException {
-    public UserNotFoundException(Integer id) {
-        super("User with id: " + id + " not found!");
-    }
-}
