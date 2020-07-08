8c033648899d4259a6cbb98a5eccc1ac6a81f071
jakub <jakubstankowski@protonmail.com>
2020-07-08 15:57:01 +0200

    Add C# backend

--- src/main/java/com/standev/lastminute/user/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.standev.lastminute.user.dao;
-
-import com.standev.lastminute.user.jpa.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDAO extends JpaRepository<User, Integer> {
-
-}
