0a0972f7c00c85250d72aa38506a66d4890fba6d
jakub <jakubstankowski@protonmail.com>
2020-07-09 18:49:54 +0200

    Add holiday preferences controller and get first mock data

---
+++ LastMinute/Controllers/HolidayPreferencesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LastMinute.Data;
+using LastMinute.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LastMinute.Controllers
+{
+    [Route("api/preferences")]
+    [ApiController]
+    public class HolidayPreferencesController : ControllerBase
+    {
+
+        private readonly MockHolidayPreferencesContext _repository = new MockHolidayPreferencesContext();
+
+        // GET: api/preferences
+        [HttpGet]
+        public IEnumerable<HolidayPreferences> GetAllPreferences()
+        {
+            return  _repository.GetAllHolidayPreferences().ToList();
+
+       }
+
+        // GET: api/HolidayPreferences/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/HolidayPreferences
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/HolidayPreferences/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
