9642003d5cde9b3ac02a76baea4b175d764dd378
jakub <jakubstankowski@protonmail.com>
2020-07-09 18:42:33 +0200

    Add holiday preference repo interface, and mock holiday preferences context

---
+++ LastMinute/Data/IHolidayPreferencesRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LastMinute.Models;
+
+namespace LastMinute.Data
+{
+    interface IHolidayPreferencesRepo
+    {
+        IEnumerable<HolidayPreferences> GetAllHolidayPreferences();
+        HolidayPreferences GetHolidayPreferenceById(int id);
+        void CreateHolidayPreference(HolidayPreferences preference);
+        void UpdateHolidayPreference(HolidayPreferences preference);
+        void DeleteHolidayPreference(HolidayPreferences preference);
+
+    }
+}
---
+++ LastMinute/Data/MockHolidayPreferencesContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LastMinute.Models;
+
+namespace LastMinute.Data
+{
+    public class MockHolidayPreferencesContext : IHolidayPreferencesRepo
+    {
+        public void CreateHolidayPreference(HolidayPreferences preference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteHolidayPreference(HolidayPreferences preference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<HolidayPreferences> GetAllHolidayPreferences()
+        {
+            var holidayPreferences = new List<HolidayPreferences>
+            {
+                new HolidayPreferences{Id=0, Country= "Poland", Title="First holiday", MinPrice = 1000, MaxPrice = 2000, Website = "r.pl"},
+                new HolidayPreferences{Id=1, Country= "Bulgaria", Title="First holiday", MinPrice = 1000, MaxPrice = 2000, Website = "itaka.pl"}
+            };
+
+            return holidayPreferences;
+        }
+
+        public HolidayPreferences GetHolidayPreferenceById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHolidayPreference(HolidayPreferences preference)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
