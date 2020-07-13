daad37e62788ad5b3db8979475fd7fadc9574515
jakub <jakubstankowski@protonmail.com>
2020-07-13 18:51:30 +0200

    Add database configuration to appsettings, and holiday preferences required annotattion

--- LastMinute/LastMinute.WebAPI/Models/HolidayPreferences.cs
+++ LastMinute/LastMinute.WebAPI/Models/HolidayPreferences.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,16 +9,23 @@ namespace LastMinute.Models
     public class HolidayPreferences
     {
 
+        [Key]
+        [Required]
         public int Id { get; set; }
 
+     
         public string Title { get; set; }
 
+        [Required]
         public string Website { get; set; }
 
+        [Required]
         public string Country { get; set; }
 
+        [Required]
         public int MinPrice { get; set; }
 
+        [Required]
         public int MaxPrice { get; set; }
     }
 }
--- LastMinute/LastMinute.WebAPI/Startup.cs
+++ LastMinute/LastMinute.WebAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +29,10 @@ namespace LastMinute
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<HolidayPreferencesContext>(opt => opt.UseSqlServer
+               (Configuration.GetConnectionString("LastMinuteConnection")));
+
+
             services.AddControllers();
             services.AddScoped<IHolidayPreferencesRepo, MockHolidayPreferencesRepo>();
         }
@@ -35,6 +40,8 @@ namespace LastMinute
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+           
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
