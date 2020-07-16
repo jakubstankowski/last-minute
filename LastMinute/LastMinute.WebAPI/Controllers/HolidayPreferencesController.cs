f39d7af929a0d72d731ea79cb90ed3f1454700b0
jakub <jakubstankowski@protonmail.com>
2020-07-16 18:34:14 +0200

    Implement update holiday preferences in controller

--- LastMinute/LastMinute.WebAPI/Controllers/HolidayPreferencesController.cs
+++ LastMinute/LastMinute.WebAPI/Controllers/HolidayPreferencesController.cs
@@ -30,7 +30,7 @@ namespace LastMinute.Controllers
             return _repository.GetAllHolidayPreferences().ToList();
         }
 
-        // GET: api/HolidayPreferences/5
+        // GET: api/preferences/5
         [HttpGet("{id}", Name = "Get")]
         public ActionResult<HolidayPreferences> Get(int id)
         {
@@ -44,7 +44,7 @@ namespace LastMinute.Controllers
             return holidayPreference;
         }
 
-        // POST: api/HolidayPreferences
+        // POST: api/preferences
         [HttpPost]
         public ActionResult Post(HolidayPreferences holidayPreferences)
         {
@@ -56,13 +56,24 @@ namespace LastMinute.Controllers
 
         }
 
-        // PUT: api/HolidayPreferences/5
+        // PUT: api/preferences/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<HolidayPreferences> Put(int id, HolidayPreferences holidayPreferences)
         {
+            var holidayPreference = _repository.GetHolidayPreferenceById(id);
+
+            if (holidayPreference == null)
+            {
+                return NotFound();
+            }
+
+
+            _repository.UpdateHolidayPreference(holidayPreference);
+
+            return holidayPreference;
         }
 
-        // DELETE: api/ApiWithActions/5
+        // DELETE: api/preferences/5
         [HttpDelete("{id}")]
         public ActionResult<HolidayPreferences> Delete(int id)
         {
