17c70fc3c06bf39c7aa54fb16af40b45ff7ad1f2
jakub <jakubstankowski@protonmail.com>
2020-07-29 16:12:53 +0200

    Add HolidayPreferences entities in core project

---
+++ LastMinute/Core/Entities/HolidayPreferences.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    class HolidayPreferences
+    {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Website { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public int MinPrice { get; set; }
+
+        [Required]
+        public int MaxPrice { get; set; }
+    }
+}
