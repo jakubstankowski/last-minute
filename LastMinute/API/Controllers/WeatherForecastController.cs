b7f953cb93531915a6c6cc67946651bb33acd521
jakub <jakubstankowski@protonmail.com>
2020-07-29 16:20:09 +0200

    Add gitignore file

--- LastMinute/API/Controllers/WeatherForecastController.cs
+++ LastMinute/API/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Test", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
