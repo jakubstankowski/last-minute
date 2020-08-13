7d2c4c34184404a8f48510798445105fd3c7762e
jakub <jakubstankowski@protonmail.com>
2020-08-13 20:48:36 +0200

    Add holiday offers entities class

---
+++ LastMinute/Core/Entities/HolidayOffers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Entities
+{
+    class HolidayOffers : BaseEntity
+    {
+        [Required]
+
+        public string Title { get; set; }
+
+        [Required]
+        public string Website { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public int MinPrice { get; set; }
+
+        [Required]
+        public int MaxPrice { get; set; }
+
+    }
+}
