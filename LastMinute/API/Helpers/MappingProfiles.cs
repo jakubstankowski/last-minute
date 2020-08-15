1da1f9981595aa2dd62c9e0e207555bce33dd461
jakub <jakubstankowski@protonmail.com>
2020-08-15 16:20:23 +0200

    LM-8 add auto mapper helpers

---
+++ LastMinute/API/Helpers/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTO;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class MappingProfiles: Profile
+    {
+
+        public MappingProfiles()
+        {
+            CreateMap<HolidayOffers, HolidayOffersToReturnDTO>();
+            CreateMap<HolidayPreferences, HolidayPreferencesToReturnDTO>();
+        }
+    }
+}
