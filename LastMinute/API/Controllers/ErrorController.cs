842154214826c496d8935ef027642dd17f45ea12
jakub <jakubstankowski@protonmail.com>
2020-08-18 17:18:36 +0200

    LM-11 add error controller, and setup middleware in startup.cs

---
+++ LastMinute/API/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : ControllerBase
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+
+    }
+}
--- LastMinute/API/Startup.cs
+++ LastMinute/API/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AutoMapper;
-
+using API.Middleware;
 
 namespace API
 {
@@ -38,10 +38,8 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
