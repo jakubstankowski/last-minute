2a92c6d687a370a89de64590964a10476ad5dddf
jakub <jakubstankowski@protonmail.com>
2020-08-20 16:56:11 +0200

    LM-9 add userID to holiday preferences

--- LastMinute/Core/Entities/AppUser.cs
+++ LastMinute/Core/Entities/AppUser.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Core.Entities
 {
-    class AppUser : IdentityUser
+    public class AppUser : IdentityUser
     {
         public HolidayPreferences HolidayPreferences { get; set; }
 
