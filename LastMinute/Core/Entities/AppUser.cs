5e794ced2f3c884f65bcee5a8b4ebd78d5fb6bb7
jakub <jakubstankowski@protonmail.com>
2020-08-20 16:53:05 +0200

    Add app user with holiday preferences relation

---
+++ LastMinute/Core/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Entities
+{
+    class AppUser : IdentityUser
+    {
+        public HolidayPreferences HolidayPreferences { get; set; }
+
+    }
+}
