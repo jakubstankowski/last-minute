882abc67640136be52aedce3c884df4664f36ad5
jakub <jakubstankowski@protonmail.com>
2020-08-21 15:21:07 +0200

    LM-9 add account controller with dependency injections

---
+++ LastMinute/API/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/offers")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+     
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+
+
+    }
+}
