10f54d60475c186b32c303c358671e993e692f54
jakub <jakubstankowski@protonmail.com>
2020-08-21 15:24:31 +0200

    LM-9 add dtos for aacount controller

---
+++ LastMinute/API/DTO/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTO
+{
+    public class UserDTO
+    {
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+    }
+}
