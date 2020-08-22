8e644f1e100a26f278d42909306639c29936feb6
jakub <jakubstankowski@protonmail.com>
2020-08-22 10:40:22 +0200

    LM-9 add token service, and interface

---
+++ LastMinute/Core/Interface/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Core.Interface
+{
+    public interface ITokenService
+    {
+        public string CreateToken(AppUser user);
+
+    }
+}
---
+++ LastMinute/Infrastructure/Services/TokenService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+using Core.Interface;
+
+namespace Infrastructure.Services
+{
+    class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
