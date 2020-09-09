d187b2f1665e4c043c4408c40db30d89737b9fbb
jakub <jakubstankowski@protonmail.com>
2020-09-09 21:07:59 +0200

    LM-12 implement get holiday offers, title and price, connect with holiday offers model

--- LastMinute/WebScrapper/ItakaWebscrapper.cs
+++ LastMinute/WebScrapper/ItakaWebscrapper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using Core.Entities;
 using Core.Interface;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -9,7 +11,8 @@ namespace WebScrapper
 {
     public class ItakaWebScrapper : IItakaWebScrapper
     {
-       IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+        IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+        List<HolidayOffers> holidayOffers = new List<HolidayOffers>();
 
         public ItakaWebScrapper()
         {
@@ -19,11 +22,39 @@ namespace WebScrapper
 
         public string GetHtmlElements()
         {
-             driver.Navigate().GoToUrl("https://www.itaka.pl/last-minute/");
-            var searchField = driver.FindElement(By.CssSelector(".current-price_value"));
+            driver.Navigate().GoToUrl("https://www.itaka.pl/last-minute/");
+            var allFindOffersTitle = driver.FindElements(By.CssSelector(".header_title a"));
+            var allFindOffersPrice = driver.FindElements(By.CssSelector(".current-price_value"));
 
-            Console.WriteLine("Get html elements: ");
-            Console.WriteLine(searchField.Text);
+
+             foreach (var title in allFindOffersTitle)
+            {
+                Console.WriteLine("title:");
+                Console.WriteLine(title.Text);
+
+                HolidayOffers offer = new HolidayOffers
+                {
+                    Title = title.Text,
+                    Price = 0,
+                    Url = "com",
+                    Website = "itaka.pl"
+                };
+                holidayOffers.Add(offer);
+            }
+
+
+            foreach (var price in allFindOffersPrice)
+            {
+                Console.WriteLine("price: ");
+                Console.WriteLine(price.Text);
+            }
+
+
+            foreach(HolidayOffers offer in holidayOffers)
+            {
+                Console.WriteLine("title in model:");
+                Console.WriteLine(offer.Title);
+            }
             return "taka webscrapper!";
 
         }
