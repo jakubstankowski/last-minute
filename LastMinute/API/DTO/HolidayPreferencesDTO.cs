c7bc257089312ecef373f99c86bc64c3b7514c95
jakub <jakubstankowski@protonmail.com>
2020-09-14 19:53:06 +0200

    LM-13 add holidayPreferencesDTO

---
+++ LastMinute/API/DTO/HolidayPreferencesDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class HolidayPreferencesDTO
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Website { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public int MinPrice { get; set; }
+
+        [Required]
+        public int MaxPrice { get; set; }
+
+    }
+}
