d08bb7e6d369872d9130be0696a4e02fbe74d386
jakub <jakubstankowski@protonmail.com>
2020-09-25 17:42:14 +0200

    LM-22 get first correct holiday offers in get holiday offer by user preferences service

--- Infrastructure/Services/HolidayOffersService.cs
+++ Infrastructure/Services/HolidayOffersService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Core.Entities;
 using Core.Interface;
@@ -10,10 +11,13 @@ namespace Infrastructure.Services
     {
         public IEnumerable<HolidayOffers> GetHolidayOffersByUserHolidayPreferences(IEnumerable<HolidayOffers> holidayOffers, IEnumerable<HolidayPreferences> holidayPreferences)
         {
-           
+            List<HolidayOffers> foundOffers = new List<HolidayOffers>();
+
+
             foreach(var preference in holidayPreferences)
             {
-
+               var foundOffer =  holidayOffers.Where(o => o.Country == preference.Country).ToList();
+               
             }
             
             throw new NotImplementedException();
