f4c34e7a46525bac1471208afca9acebb54d96e6
jakub <jakubstankowski@protonmail.com>
2020-10-05 19:20:57 +0200

    LM-28 implement basic login page and send data to the endpoint

--- Frontend/src/app/auth/login/login.component.ts
+++ Frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
-import { Title } from '@angular/platform-browser';
-import { EMPTY, of } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
+import {Title} from '@angular/platform-browser';
+import {EMPTY, of} from 'rxjs';
 import 'rxjs/add/operator/delay';
 
-import { AuthenticationService } from '../../core/services/auth.service';
-import { NotificationService } from '../../core/services/notification.service';
+import {AuthenticationService} from '../../core/services/auth.service';
+import {NotificationService} from '../../core/services/notification.service';
 
 @Component({
     selector: 'app-login',
@@ -19,52 +19,39 @@ export class LoginComponent implements OnInit {
     loading: boolean;
 
     constructor(private router: Router,
-        private titleService: Title,
-        private notificationService: NotificationService,
-        private authenticationService: AuthenticationService) {
+                private titleService: Title,
+                private notificationService: NotificationService,
+                private authenticationService: AuthenticationService) {
     }
 
     ngOnInit() {
         this.titleService.setTitle('Last Minute - Login');
-        this.authenticationService.logout();
         this.createForm();
     }
 
     private createForm() {
-        const savedUserEmail = localStorage.getItem('savedUserEmail');
-
         this.loginForm = new FormGroup({
-            email: new FormControl(savedUserEmail, [Validators.required, Validators.email]),
-            password: new FormControl('', Validators.required),
-            rememberMe: new FormControl(savedUserEmail !== null)
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', Validators.required)
         });
     }
 
-    login() {
+    login(): void {
         const email = this.loginForm.get('email').value;
         const password = this.loginForm.get('password').value;
-        const rememberMe = this.loginForm.get('rememberMe').value;
-
         this.loading = true;
         this.authenticationService
             .login(email.toLowerCase(), password)
             .subscribe(
                 data => {
-                    if (rememberMe) {
-                        localStorage.setItem('savedUserEmail', email);
-                    } else {
-                        localStorage.removeItem('savedUserEmail');
-                    }
+                    this.loading = true;
                     this.router.navigate(['/']);
                 },
                 error => {
-                    this.notificationService.openSnackBar(error.error);
+                    console.log('Error: ', error);
+                    this.notificationService.openSnackBar(error.error.message);
                     this.loading = false;
                 }
             );
-    }
-
-    resetPassword() {
-        this.router.navigate(['/auth/password-reset-request']);
     }
 }
--- Frontend/src/app/core/services/auth.service.ts
+++ Frontend/src/app/core/services/auth.service.ts
@@ -1,41 +1,28 @@
-import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import {Injectable, Inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 import * as jwt_decode from 'jwt-decode';
 import * as moment from 'moment';
 import 'rxjs/add/operator/delay';
 
-import { environment } from '../../../environments/environment';
-import { of, EMPTY } from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {of, EMPTY} from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthenticationService {
+    baseUrl = environment.apiUrl;
 
     constructor(private http: HttpClient,
-        @Inject('LOCALSTORAGE') private localStorage: Storage) {
+                @Inject('LOCALSTORAGE') private localStorage: Storage) {
     }
 
     login(email: string, password: string) {
-        return of(true).delay(1000)
-            .pipe(map((/*response*/) => {
-                // set token property
-                // const decodedToken = jwt_decode(response['token']);
-
-                // store email and jwt token in local storage to keep user logged in between page refreshes
-                this.localStorage.setItem('currentUser', JSON.stringify({
-                    token: 'aisdnaksjdn,axmnczm',
-                    isAdmin: true,
-                    email: 'john.doe@gmail.com',
-                    id: '12312323232',
-                    alias: 'john.doe@gmail.com'.split('@')[0],
-                    expiration: moment().add(1, 'days').toDate(),
-                    fullName: 'John Doe'
-                }));
-
-                return true;
-            }));
+        return this.http.post(this.baseUrl + 'auth/login', {
+            email: email,
+            password: password
+        });
     }
 
     logout(): void {
