ba7e185ccb77c0e6b2a7b61d67f87eb97f967c90
jakub <jakubstankowski@protonmail.com>
2020-10-13 16:35:23 +0200

    LM-35 show holiday-preferences-list.component.html component in holiday-offers-list.component.html

--- Frontend/src/app/holiday-offers/holiday-offers.module.ts
+++ Frontend/src/app/holiday-offers/holiday-offers.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {HolidayOffersListComponent} from './holiday-offers-list/holiday-offers-list.component';
 import {HolidayOffersRoutingModule} from './holiday-offers-routing.module';
 import {SharedModule} from '../shared/shared.module';
+import {HolidayPreferencesListComponent} from '../holiday-preferences/holiday-preferences-list/holiday-preferences-list.component';
 
 
 @NgModule({
     declarations: [
-        HolidayOffersListComponent
+        HolidayOffersListComponent,
+        HolidayPreferencesListComponent
     ],
     imports: [
         CommonModule,
