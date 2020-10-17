747c6e19dbbc2fb579984a995cf28fe160ff2a43
jakub <jakubstankowski@protonmail.com>
2020-10-17 12:59:35 +0200

    LM-41 get first offers with webAPIClient from tui.pl website

--- WebAPIClient/Program.cs
+++ WebAPIClient/Program.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WebAPIClient
 {
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
+
+
 
         static async Task Main(string[] args)
         {
@@ -20,23 +24,24 @@ namespace WebAPIClient
 
         private static async Task ProcessRepositories()
         {
+            // client.BaseAddress = new Uri("http://localhost:5000/api/auth/login");
+
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            string content = "{\"childrenBirthdays\":[],\"durationFrom\":6,\"durationTo\":14,\"filters\":[{\"filterId\":\"additionalType\",\"selectedValues\":[\"GT03#TUZ-LAST25\"]}],\"metaData\":{\"page\":0,\"pageSize\":30,\"sorting\":\"flightDate\"},\"numberOfAdults\":2,\"offerType\":\"BY_PLANE\",\"site\":\"last-minute?pm_source=MENU&pm_name=Last_Minute\"}";
+            var result = await client.PostAsync("https://www.tui.pl/search/offers", new StringContent(content, Encoding.UTF8, "application/json"));
 
-            foreach (var repo in repositories)
-            {
-                Console.WriteLine(repo.name);
-            }
-                
+            string resultContent = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultContent);
 
 
         }
 
 
+
     }
+
+
 }
+
---
+++ WebAPIClient/Tui.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAPIClient
+{
+    class Tui
+    {
+        public string Offers { get; set; }
+    }
+}
