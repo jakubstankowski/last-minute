a0802661625cc6bf84761bba20d62b648a8e57a2
jakub <jakubstankowski@protonmail.com>
2020-10-18 18:18:05 +0200

    LM-41 implement base collect wakacje data

--- WebAPIClient/GenericWebAPIClient.cs
+++ WebAPIClient/GenericWebAPIClient.cs
@@ -124,5 +124,19 @@ namespace WebAPIClient
             }
 
         }
+
+        public async Task CollectWakacjeDataAsync()
+        {
+
+            string postBody = "[{\"method\":\"search.tripsSearch\",\"params\":{\"brand\":\"WAK\",\"limit\":10,\"priceHistory\":1,\"imageSizes\":[\"570,428\"],\"flatArray\":true,\"multiSearch\":true,\"withHotelRate\":1,\"withPromoOffer\":1,\"recommendationVersion\":\"noTUI\",\"type\":\"tours\",\"firstMinuteTui\":false,\"countryId\":[\"37\",\"33\",\"99\",\"74\",\"65\",\"16\"],\"regionId\":[],\"cityId\":[],\"hotelId\":[],\"roundTripId\":[],\"cruiseId\":[],\"searchType\":\"lastminute\",\"offersAttributes\":[],\"alternative\":{\"countryId\":[],\"regionId\":[],\"cityId\":[]},\"query\":{\"campTypes\":[],\"qsVersion\":0,\"qsVersionLast\":0,\"tab\":false,\"candy\":false,\"pok\":null,\"flush\":false,\"tourOpAndCode\":null,\"obj_code\":null,\"obj_type\":null,\"catalog\":null,\"roomType\":null,\"test\":null,\"year\":null,\"month\":null,\"rangeDate\":null,\"withoutLast\":0,\"category\":false,\"not - attribute\":false,\"pageNumber\":1,\"departureDate\":\"2020 - 10 - 14\",\"arrivalDate\":\"2022 - 05 - 01\",\"departure\":null,\"type\":[1],\"duration\":{\"min\":7,\"max\":28},\"minPrice\":null,\"maxPrice\":null,\"service\":[],\"firstminute\":null,\"attribute\":[],\"promotion\":[],\"tourId\":null,\"search\":null,\"minCategory\":null,\"maxCategory\":50,\"sort\":13,\"order\":1,\"rank\":null,\"withoutTours\":[],\"withoutCountry\":[],\"withoutTrips\":[],\"rooms\":[{\"adult\":2,\"kid\":0,\"ages\":[],\"inf\":null}],\"offerCode\":null}}}]";
+
+            var postResult = await client.PostAsync("https://www.wakacje.pl/v2/api/offers", new StringContent(postBody, Encoding.UTF8, "application/json"));
+            string stringResult = await postResult.Content.ReadAsStringAsync();
+            Console.WriteLine(stringResult);
+
+        }
+
+
+
     }
 }
