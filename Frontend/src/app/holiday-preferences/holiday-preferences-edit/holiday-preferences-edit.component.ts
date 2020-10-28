5110aab7219540dccbd72352ff86c06b7ee302a2
jakub <jakubstankowski@protonmail.com>
2020-10-28 18:35:15 +0100

    LM-52 mapping selected website and send to the endpoint

--- Frontend/src/app/holiday-preferences/holiday-preferences-edit/holiday-preferences-edit.component.ts
+++ Frontend/src/app/holiday-preferences/holiday-preferences-edit/holiday-preferences-edit.component.ts
@@ -26,47 +26,69 @@ export class HolidayPreferencesEditComponent implements OnInit {
     ngOnInit() {
         this.getPreference();
         this.createForm();
-        // this.toppings.setValue([{topping: 'test'}]);
     }
 
     private createForm() {
         this.preferenceForm = new FormGroup({
             minPrice: new FormControl('', Validators.required),
             maxPrice: new FormControl('', Validators.required),
-            websites: new FormControl('', Validators.required)
+            websites: new FormControl([])
         });
     }
 
     getPreference() {
         this.holidayPreferencesService.getHolidayPreference()
-            .subscribe(preferences => {
-                this.preferenceForm.setValue(preferences);
-                for (let i = 0; i < preferences.websites.length; i++) {
-                    this.selectedWebsites.push(preferences.websites[i]['website']);
+            .subscribe(
+                preferences => {
+                    this.preferenceForm.patchValue({
+                        minPrice: preferences.minPrice,
+                        maxPrice: preferences.maxPrice
+                    });
+
+                    for (let i = 0; i < preferences.websites.length; i++) {
+                        this.selectedWebsites.push(preferences.websites[i]['website']);
+                    }
+                },
+                error => {
+                    this.notificationService.openSnackBar(error.error.message);
+                    throw new Error(error);
                 }
-            });
+            );
     }
 
 
     updatePreference() {
-        console.log('form value : ', this.preferenceForm.value);
-        /* this.holidayPreferencesService
-             .updateHolidayPreference(this.preferenceForm.value)
-             .subscribe(
-                 data => {
-                     this.router.navigate(['/holiday-preferences']);
-                 },
-                 error => {
-                     console.log('Error: ', error);
-                     this.notificationService.openSnackBar(error.error.message);
-                 }
-             );*/
+        const websitesList: Object[] = []
+
+        for (let i = 0; i < this.selectedWebsites.length; i++) {
+            websitesList.push({
+                website: this.selectedWebsites[i]
+            });
+        }
+
+        this.preferenceForm.patchValue({
+            websites: websitesList,
+        });
+
+
+        this.holidayPreferencesService
+            .updateHolidayPreference(this.preferenceForm.value)
+            .subscribe(
+                data => {
+                    this.router.navigate(['/holiday-preferences']);
+                },
+                error => {
+                    this.notificationService.openSnackBar(error.error.message);
+                    throw new Error(error);
+                }
+            );
     }
 
     updateWebsites(website: string) {
         if (this.selectedWebsites.includes(website)) {
             return this.selectedWebsites.splice(this.selectedWebsites.indexOf(website), 1);
         }
+
         this.selectedWebsites.push(website);
     }
 }
