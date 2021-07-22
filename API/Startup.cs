658af1a9645e2951353d617a3b93511246dead7c
jakub <jakubstankowski@protonmail.com>
2021-07-22 21:38:23 +0200

    Remove postgress migration, add SQL provider

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,45 +58,9 @@ namespace API
 
             services.AddControllers();
 
-            /*   services.AddDbContext<DataContext>(opt =>
-               {
-                   var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-                   string connStr;
-
-
-                   if (env == "Development")
-                   {
-                       connStr = Configuration.GetConnectionString("LastMinuteConnection");
-                   }
-                   else
-                   {
-                       var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-                       connUrl = connUrl.Replace("postgress://", string.Empty);
-                       var pgUserPass = connUrl.Split("@")[0];
-                       var pgHostPortDb = connUrl.Split("@")[1];
-                       var pgHostPort = pgHostPortDb.Split("/")[0];
-                       var pgDb = pgHostPortDb.Split("/")[1];
-                       var pgUser = pgUserPass.Split(":")[0];
-                       var pgPass = pgUserPass.Split(":")[1];
-                       var pgHost = pgHostPort.Split(":")[0];
-                       var pgPort = pgHostPort.Split(":")[1];
-
-                       connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
-
-                       Console.WriteLine("connStr: " + connStr);
-
-                   }
-                   opt.UseNpgsql(connStr);
-               }
-
-
-               );*/
-
             services.AddDbContext<DataContext>(opt =>
-           opt.UseNpgsql(
-               Configuration.GetConnectionString("LastMinuteConnection")));
+          .UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
 
 
             services.AddIdentity<AppUser, IdentityRole>()
