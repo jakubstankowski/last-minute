eeb58eb60ba003bac38f911397690e26eaa7b47b
jakub <jakubstankowski@protonmail.com>
2021-07-24 12:44:46 +0200

    LM-76 add days number migration

---
+++ Infrastructure/Data/Migrations/20210724104216_AddDaysNumberToHolidayOffers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class AddDaysNumberToHolidayOffers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DaysNumber",
+                table: "HolidayOffers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DaysNumber",
+                table: "HolidayOffers");
+        }
+    }
+}
