df87b2262995aa4564440271286614ee8d0785f7
jakub <jakubstankowski@protonmail.com>
2021-07-24 14:41:53 +0200

    EL-44 add transport variable to holiday offer entity

--- Core/Entities/HolidayOffers.cs
+++ Core/Entities/HolidayOffers.cs
@@ -21,6 +21,9 @@ namespace Core.Entities
         public string Country { get; set; }
 
         [Required]
+        public string Transport { get; set; }
+
+        [Required]
         public int Price { get; set; }
 
         [Required]
